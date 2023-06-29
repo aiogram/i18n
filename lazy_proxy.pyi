2c05fce58c5929f0d3c0b3fa1e20a4ca8d7c2b5b
nullmatawasoradesu <119107850+wakaree@users.noreply.github.com>
2023-06-29 14:15:37 +0300

    Removed stub

--- lazy_proxy.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any, Optional
-
-from magic_filter import MagicFilter
-
-
-class LazyProxy(str):
-    def __init__(self, key: str, magic: Optional[MagicFilter] = None, **kwargs: Any) -> None:
-        ...
