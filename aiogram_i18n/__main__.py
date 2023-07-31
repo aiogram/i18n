8f023d79f978e692b7a5287adc4d6f0a082a640e
andrew000 <11490628+andrew000@users.noreply.github.com>
2023-07-31 21:02:36 +0300

    ADD FluentMultipleKeyParser ADD BaseFluentKeyParser Refactor FluentKeyParser to use Path instead of str Fix `locale=` in i18n.get(...) not to be gathered by parser
    Now Parsers accepts `locales`, which will create sub-dirs (eg. `uk`, `en`). If locales is None - no sub-dirs will be created.
    
    FluentMultipleKeyParser parses locale-tokens in code and split them by separator. [`token.split(maxsplit=1)`]
    First element of local-token is used to create sub-ftl-files for them.
    Also u can pass locales and create sub-dirs with sub-ftl-files.
    
    The code was formatted by Black + Ruff.

--- aiogram_i18n/__main__.py
+++ aiogram_i18n/__main__.py
@@ -1,6 +1,5 @@
-
 from pathlib import Path
-from typing import Dict, Callable, Tuple, Sequence
+from typing import Dict, Callable, Tuple, Sequence, Union
 
 import click
 
@@ -40,21 +39,74 @@ def stub(input_files: Tuple[str, ...], output_file: str) -> None:
 
 
 @main.command(help="Extract all used fluent keys from code")  # type: ignore[misc]
-@click.option("-i", "--input-dirs", required=True, multiple=True)
-@click.option("-o", "--output-file", required=True)
-@click.option("-k", "--i18n-keys", default=["i18n", "L"], multiple=True, show_default=True)
-@click.option("-s", "--separator", default="-", show_default=True)
-@click.option("-ed", "--exclude-dirs", default=["venv"], multiple=True)
-@click.option("-ek", "--exclude-keys", multiple=True)
+@click.option(
+    "-i",
+    "--input-dirs",
+    multiple=True,
+    required=True,
+    type=click.Path(exists=True),
+)
+@click.option(
+    "-o",
+    "--output-file",
+    required=True,
+    type=click.Path(),
+)
+@click.option(
+    "-k",
+    "--i18n-keys",
+    default=["i18n", "L"],
+    multiple=True,
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--separator",
+    default="-",
+    show_default=True,
+)
+@click.option(
+    "-l",
+    "--locales",
+    multiple=True,
+)
+@click.option(
+    "-ed",
+    "--exclude-dirs",
+    default=(Path("venv"), Path(".venv")),
+    multiple=True,
+    type=click.Path(),
+)
+@click.option(
+    "-ek",
+    "--exclude-keys",
+    multiple=True,
+)
+@click.option(
+    "-cm",
+    "--create-missing-dirs",
+    is_flag=True,
+    default=False,
+)
 def extract(
-    input_dirs: Tuple[str, ...], output_file: str,
-    i18n_keys: Tuple[str, ...], separator: str,
-    exclude_dirs: Tuple[str, ...], exclude_keys: Tuple[str, ...]
+        input_dirs: Tuple[str, ...],
+        output_file: str,
+        i18n_keys: Tuple[str, ...],
+        separator: str,
+        locales: Union[Tuple[str, ...], None],
+        exclude_dirs: Tuple[str, ...],
+        exclude_keys: Tuple[str, ...],
+        create_missing_dirs: bool,
 ) -> None:
     from aiogram_i18n import I18nContext
     from aiogram_i18n.utils.fluent_extract import FluentKeyParser
 
-    exclude_dirs += "__pycache__",
+    input_dirs = tuple(Path(input_dir) for input_dir in input_dirs)
+    output_file = Path(output_file)
+
+    exclude_dirs = tuple(Path(exclude_dir) for exclude_dir in exclude_dirs)
+    exclude_dirs += (Path("__pycache__"),)
+
     exclude_keys += tuple(
         key for key, value in I18nContext.__dict__.items() if callable(value)
     )
@@ -64,11 +116,109 @@ def extract(
         output_file=output_file,
         i18n_keys=i18n_keys,
         separator=separator,
+        locales=locales,
         exclude_dirs=exclude_dirs,
-        exclude_keys=exclude_keys
+        exclude_keys=exclude_keys,
     )
-    fkp.run()
+    fkp.run(create_missing_dirs=create_missing_dirs)
 
 
-if __name__ == '__main__':
+@main.command(
+    help="Extract all used fluent keys from code and split them by files")  # type: ignore[misc]
+@click.option(
+    "-i",
+    "--input-paths",
+    multiple=True,
+    required=True,
+    type=click.Path(exists=True),
+)
+@click.option(
+    "-o",
+    "--output-dir",
+    required=True,
+    type=click.Path(),
+)
+@click.option(
+    "-k",
+    "--i18n-keys",
+    default=["i18n", "L"],
+    multiple=True,
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--separator",
+    default="-",
+    show_default=True,
+)
+@click.option(
+    "-l",
+    "--locales",
+    multiple=True,
+)
+@click.option(
+    "-ed",
+    "--exclude-dirs",
+    default=(Path("venv"), Path(".venv")),
+    multiple=True,
+    type=click.Path(),
+)
+@click.option(
+    "-ek",
+    "--exclude-keys",
+    multiple=True,
+)
+@click.option(
+    "-cm",
+    "--create-missing-dirs",
+    is_flag=True,
+    default=False,
+)
+def multiple_extract(
+        input_paths: Tuple[str, ...],
+        output_dir: str,
+        i18n_keys: Tuple[str, ...],
+        separator: str,
+        locales: Union[Tuple[str, ...], None],
+        exclude_dirs: Tuple[str, ...],
+        exclude_keys: Tuple[str, ...],
+        create_missing_dirs: bool,
+) -> None:
+    """
+    Extracts all used fluent keys from code and saves them in the specified files.
+
+    Keys should be separated by <type>-<key_name>.
+    Keys will be saved in multiple files, depending on the type.
+    The type is specified by the first part of the key.
+
+    Example:
+        - "buttons-cancel" -> "buttons" is the type, "cancel" is the key name.
+        - "checker-no-rights" -> "checker" is the type, "no-rights" is the key name.
+    """
+    from aiogram_i18n import I18nContext
+    from aiogram_i18n.utils.fluent_extract import FluentMultipleKeyParser
+
+    input_paths = tuple(Path(input_path) for input_path in input_paths)
+    output_dir = Path(output_dir)
+
+    exclude_dirs = tuple(Path(exclude_dir) for exclude_dir in exclude_dirs)
+    exclude_dirs += (Path("__pycache__"),)
+
+    exclude_keys += tuple(
+        key for key, value in I18nContext.__dict__.items() if callable(value)
+    )
+
+    fkp = FluentMultipleKeyParser(
+        input_paths=input_paths,
+        output_dir=output_dir,
+        i18n_keys=i18n_keys,
+        separator=separator,
+        locales=locales,
+        exclude_dirs=exclude_dirs,
+        exclude_keys=exclude_keys,
+    )
+    fkp.run(create_missing_dirs=create_missing_dirs)
+
+
+if __name__ == "__main__":
     main()
--- aiogram_i18n/utils/fluent_extract/__init__.py
+++ aiogram_i18n/utils/fluent_extract/__init__.py
@@ -1,13 +1,18 @@
-
 from aiogram_i18n.exceptions import NoModuleError
 
 try:
-    from .parser import FluentKeyParser
-    from .models import FluentKeywords, FluentTemplate, FluentMatch
+    from .base import BaseFluentKeyParser
+    from .parser import FluentKeyParser, FluentMultipleKeyParser
+    from .models import FluentKeywords, FluentMatch, FluentTemplate, FluentTemplateDir
 except ImportError:
     raise NoModuleError(name="Fluent key extractor", module_name="libcst")
 
-
 __all__ = [
-    "FluentKeyParser", "FluentKeywords", "FluentTemplate", "FluentMatch"
+    "BaseFluentKeyParser",
+    "FluentKeyParser",
+    "FluentKeywords",
+    "FluentMatch",
+    "FluentMultipleKeyParser",
+    "FluentTemplate",
+    "FluentTemplateDir",
 ]
