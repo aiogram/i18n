64348048f86a2f2760f427f1c7e67fc131eb4e2e
andrew000 <11490628+andrew000@users.noreply.github.com>
2023-09-27 02:10:57 +0300

    Rework TMP directories for tests

--- tests/test_multiple_extract.py
+++ tests/test_multiple_extract.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any, Final
 
 import pytest
+from _pytest.legacypath import TempdirFactory
 from babel import UnknownLocaleError
 from pytest_asyncio import fixture
 from pytest_lazyfixture import lazy_fixture
@@ -11,38 +12,44 @@ from aiogram_i18n.utils.fluent_extract import FluentMultipleKeyParser
 from tests.check_translations import _check_translations
 
 TEST_CODE_DIR: Final[Path] = Path(__file__).parent.joinpath("data", "test_code").absolute()
-NO_LOCALES_OUTPUT_DIR: Final[Path] = Path(__file__).parent.joinpath("no_locales_output").absolute()
-MULTIPLE_LOCALES_OUTPUT_DIR: Final[Path] = (
-    Path(__file__).parent.joinpath("multiple_locales_output").absolute()
-)
+
+
+@pytest.fixture(scope="function")
+def no_locales_output(tmpdir_factory: TempdirFactory) -> Path:
+    return Path(*tmpdir_factory.mktemp("no_locales_output").parts())
+
+
+@pytest.fixture(scope="function")
+def multiple_locales_output(tmpdir_factory: TempdirFactory) -> Path:
+    return Path(*tmpdir_factory.mktemp("multiple_locales_output").parts())
 
 
 @fixture(scope="function")
-def fluent_runtime_core_no_locales() -> BaseCore[Any]:
+def fluent_runtime_core_no_locales(no_locales_output: Path) -> BaseCore[Any]:
     from aiogram_i18n.cores import FluentRuntimeCore
 
-    return FluentRuntimeCore(path=NO_LOCALES_OUTPUT_DIR, use_isolating=False)
+    return FluentRuntimeCore(path=no_locales_output, use_isolating=False)
 
 
 @fixture(scope="function")
-def fluent_runtime_core_multiple_locales() -> BaseCore[Any]:
+def fluent_runtime_core_multiple_locales(multiple_locales_output: Path) -> BaseCore[Any]:
     from aiogram_i18n.cores import FluentRuntimeCore
 
-    return FluentRuntimeCore(path=MULTIPLE_LOCALES_OUTPUT_DIR / "{locale}", use_isolating=False)
+    return FluentRuntimeCore(path=multiple_locales_output / "{locale}", use_isolating=False)
 
 
 @fixture(scope="function")
-def fluent_compile_core_no_locales() -> BaseCore[Any]:
+def fluent_compile_core_no_locales(no_locales_output: Path) -> BaseCore[Any]:
     from aiogram_i18n.cores import FluentCompileCore
 
-    return FluentCompileCore(path=NO_LOCALES_OUTPUT_DIR, use_isolating=False)
+    return FluentCompileCore(path=no_locales_output, use_isolating=False)
 
 
 @fixture(scope="function")
-def fluent_compile_core_multiple_locales() -> BaseCore[Any]:
+def fluent_compile_core_multiple_locales(multiple_locales_output: Path) -> BaseCore[Any]:
     from aiogram_i18n.cores import FluentCompileCore
 
-    return FluentCompileCore(path=MULTIPLE_LOCALES_OUTPUT_DIR / "{locale}", use_isolating=False)
+    return FluentCompileCore(path=multiple_locales_output / "{locale}", use_isolating=False)
 
 
 @pytest.mark.parametrize(
@@ -53,10 +60,10 @@ def fluent_compile_core_multiple_locales() -> BaseCore[Any]:
     ],
 )
 @pytest.mark.asyncio
-async def test_multiple_extract(i18n: BaseCore[Any]) -> None:
+async def test_multiple_extract(i18n: BaseCore[Any], multiple_locales_output: Path) -> None:
     fkp = FluentMultipleKeyParser(
         input_paths=(TEST_CODE_DIR,),
-        output_dir=MULTIPLE_LOCALES_OUTPUT_DIR,
+        output_dir=multiple_locales_output,
         i18n_keys=["i18n", "L", "I18NFormat"],
         separator="-",
         locales=["en", "uk"],
@@ -66,22 +73,19 @@ async def test_multiple_extract(i18n: BaseCore[Any]) -> None:
     )
     fkp.run(create_missing_dirs=True)
 
-    try:
-        await i18n.startup()
-        assert set(i18n.available_locales) == {"en", "uk"}
-        assert MULTIPLE_LOCALES_OUTPUT_DIR.joinpath("en").exists()
-        assert MULTIPLE_LOCALES_OUTPUT_DIR.joinpath("uk").exists()
-        _check_translations(i18n)
-    finally:
-        __import__("shutil").rmtree(MULTIPLE_LOCALES_OUTPUT_DIR)
+    await i18n.startup()
+    assert set(i18n.available_locales) == {"en", "uk"}
+    assert multiple_locales_output.joinpath("en").exists()
+    assert multiple_locales_output.joinpath("uk").exists()
+    _check_translations(i18n)
 
 
 @pytest.mark.asyncio
 @pytest.mark.xfail(raises=FileNotFoundError)
-async def test_file_not_found_exception():
+async def test_file_not_found_exception(multiple_locales_output: Path):
     fkp = FluentMultipleKeyParser(
         input_paths=(TEST_CODE_DIR / "._ERROR_PATH",),
-        output_dir=MULTIPLE_LOCALES_OUTPUT_DIR,
+        output_dir=multiple_locales_output,
         i18n_keys=["i18n", "L", "I18NFormat"],
         separator="-",
         locales=["en", "uk"],
@@ -101,10 +105,10 @@ async def test_file_not_found_exception():
 )
 @pytest.mark.asyncio
 @pytest.mark.xfail(raises=(UnknownLocaleError, TypeError))
-async def test_unknown_locale_error(i18n: BaseCore[Any]):
+async def test_unknown_locale_error(i18n: BaseCore[Any], no_locales_output: Path):
     fkp = FluentMultipleKeyParser(
         input_paths=(TEST_CODE_DIR,),
-        output_dir=NO_LOCALES_OUTPUT_DIR,
+        output_dir=no_locales_output,
         i18n_keys=["i18n", "L", "I18NFormat"],
         separator="-",
         locales=[],
@@ -115,9 +119,4 @@ async def test_unknown_locale_error(i18n: BaseCore[Any]):
     fkp.run(create_missing_dirs=True)
 
     assert i18n.available_locales == ()
-    try:
-        await i18n.startup()
-
-    finally:
-        __import__("shutil").rmtree(NO_LOCALES_OUTPUT_DIR)
-        pass
+    await i18n.startup()
