f924be02fc4846c415a697a41ade1bf2398c790c
RootShinobi <111008396+RootShinobi@users.noreply.github.com>
2024-02-27 18:00:29 +0200

    fix init

--- aiogram_i18n/__init__.py
+++ aiogram_i18n/__init__.py
@@ -1,6 +1,6 @@
 from .__meta__ import __version__
 from .context import I18nContext
-from .lazy import LazyFactory, LazyProxy, LazyFilter
+from .lazy import LazyFactory, LazyFilter, LazyProxy
 from .middleware import I18nMiddleware
 
 L = LazyFactory()
