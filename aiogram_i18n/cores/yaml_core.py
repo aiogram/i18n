e745f09c68c610035493d30d822d949aafa2cb8c
Сергій 🇺🇦 <140353988+klaymov@users.noreply.github.com>
2025-04-16 09:56:14 +0300

    pre-update

--- aiogram_i18n/cores/yaml_core.py
+++ aiogram_i18n/cores/yaml_core.py
@@ -1,9 +1,13 @@
-import yaml
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
 
 from aiogram_i18n.cores.base import BaseCore
-from aiogram_i18n.exceptions import KeyNotFoundError
+from aiogram_i18n.exceptions import KeyNotFoundError, NoModuleError
+
+try:
+    from yaml import safe_load
+except ImportError as e:
+    raise NoModuleError(name="YamlCore", module_name="pyyaml") from e
 
 
 class YamlCore(BaseCore[Dict[str, Any]]):
@@ -17,34 +21,37 @@ class YamlCore(BaseCore[Dict[str, Any]]):
         super().__init__(path=path, default_locale=default_locale, locales_map=locales_map)
         self.raise_key_error = raise_key_error
 
-    def _extract_locales(self, path: Path) -> list[str]:
-        return [p.stem for p in path.glob("*.yaml")] + [p.stem for p in path.glob("*.yml")]
+    # з цими функціями працює, але з оригінальними баг
+    # def _extract_locales(self, path: Path) -> list[str]:
+    #     return [p.stem for p in path.glob("*.yaml")] + [p.stem for p in path.glob("*.yml")]
 
-    def _find_locales(
-        self,
-        path: Path,
-        locales: list[str],
-        extensions: tuple[str, ...] = (".yaml", ".yml")
-    ) -> Dict[str, list[Path]]:
-        found = {}
-        for loc in locales:
-            found[loc] = []
-            for ext in extensions:
-                file_path = path / f"{loc}{ext}"
-                if file_path.is_file():
-                    found[loc].append(file_path)
-        return found
+    # def _find_locales(
+    #     self,
+    #     path: Path,
+    #     locales: list[str],
+    #     extensions: tuple[str, ...] = (".yaml", ".yml")
+    # ) -> Dict[str, list[Path]]:
+    #     found = {}
+    #     for loc in locales:
+    #         found[loc] = []
+    #         for ext in extensions:
+    #             file_path = path / f"{loc}{ext}"
+    #             if file_path.is_file():
+    #                 found[loc].append(file_path)
+    #     return found
 
     def find_locales(self) -> Dict[str, Dict[str, Any]]:
-        translations = {}
-        locales = self._extract_locales(Path(self.path))
-        for locale, files in self._find_locales(Path(self.path), locales).items():
-            data = {}
-            for file in files:
-                with file.open(encoding="utf-8") as f:
-                    content = yaml.safe_load(f) or {}
-                data.update(content)
-            translations[locale] = data
+        translations: Dict[str, Dict[str, Any]] = {}
+        locales = self._extract_locales(self.path)
+        for file_ext in (".yaml", ".yml"):
+            for locale, paths in self._find_locales(self.path, locales, file_ext).items():
+
+                if locale not in translations:
+                    translations[locale] = {}
+                for file in paths:
+                    with file.open(encoding="utf-8") as f:
+                        content = safe_load(f) or {}
+                    translations[locale].update(content)
         return translations
 
     def get(self, message_id: str, locale: Optional[str] = None, /, **kwargs: Any) -> str:
@@ -59,6 +66,6 @@ class YamlCore(BaseCore[Dict[str, Any]]):
                 raise KeyNotFoundError(message_id) from None
             return message_id
         try:
-            return text.format(**kwargs)
+            return text.format_map(kwargs)
         except Exception:
             return text
