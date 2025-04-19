3396c3f4fdaf964c35e750ba6997ae73a18da9ed
lores <lores1337.main@gmail.com>
2025-04-19 18:51:33 +0300

    fix typo in key_seperator parameter name in Jinja2Core

--- aiogram_i18n/cores/jinja2_core.py
+++ aiogram_i18n/cores/jinja2_core.py
@@ -22,7 +22,7 @@ class Jinja2Core(BaseCore[Dict]):
         raise_key_error: bool = True,
         use_td: bool = True,
         locales_map: Optional[Dict[str, str]] = None,
-        key_seperator: str = "-",
+        key_separator: str = "-",
     ) -> None:
         super().__init__(path=path, default_locale=default_locale, locales_map=locales_map)
         self.environment = environment or Environment(autoescape=True)
@@ -30,7 +30,7 @@ class Jinja2Core(BaseCore[Dict]):
             for name, func in td.functions.items():
                 self.environment.filters[name.lower()] = func
         self.raise_key_error = raise_key_error
-        self.key_seperator = key_seperator
+        self.key_separator = key_separator
 
     def get(self, message_id: str, locale: Optional[str] = None, /, **kwargs: Any) -> str:
         locale = self.get_locale(locale=locale)
@@ -55,6 +55,6 @@ class Jinja2Core(BaseCore[Dict]):
                     content = self.environment.from_string(f.read())
                     relative_file_path = file_path.relative_to(self.path / locale)
                     parts = relative_file_path.with_suffix("").parts
-                    key = self.key_seperator.join(parts)
+                    key = self.key_separator.join(parts)
                     translations[locale][key] = content
         return translations
